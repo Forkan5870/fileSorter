75bd38a03a2ffa2bb13b20c833cc8021f1b233a6
Samuel Orellana Mateo <samuel.orellanamateo@duke.edu>
2024-05-15 15:39:32 +0200

    Testing file finished

--- llm.py
+++ llm.py
@@ -3,7 +3,9 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 
-def promptLLM(prompt, model="llama3", system=False):
+# This function is a simplified version of the Bot class from ollamaBot.py
+
+def promptLLM(prompt, model="llama2", system=False):
 
     output_parser = StrOutputParser()
 
@@ -27,3 +29,6 @@ def promptLLM(prompt, model="llama3", system=False):
     output = chain.invoke({"input": prompt })
 
     print(output.strip())
+
+
+promptLLM("Hello! Create a one-sentence story.")
